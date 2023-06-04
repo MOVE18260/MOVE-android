df31b8be22126497f368e6df02d89d5a08f156c1
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-06-04 20:47:28 +0900

    refactor: location 관련 로직 MVVM 패턴 적용

--- app/src/main/java/dev/ehyeon/move18260_googlemapsandroidapiexample/Fragment/MapFragment.java
+++ app/src/main/java/dev/ehyeon/move18260_googlemapsandroidapiexample/Fragment/MapFragment.java
@@ -1,31 +1,26 @@
 package dev.ehyeon.move18260_googlemapsandroidapiexample.Fragment;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.gms.maps.SupportMapFragment;
 
 import dev.ehyeon.move18260_googlemapsandroidapiexample.R;
 import dev.ehyeon.move18260_googlemapsandroidapiexample.data.location.LocationSensor;
+import dev.ehyeon.move18260_googlemapsandroidapiexample.presentation.viewmodel.LocationViewModel;
 
 public class MapFragment extends Fragment {
 
-    private static final long MIN_TIME_INTERVAL = 1000; // 1초마다 TextView 업데이트
-
-    private LocationSensor locationSensor;
-
-    private Handler handler;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        locationSensor = LocationSensor.getLocationSensor();
-
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
         // DEBUG 용도
@@ -33,18 +28,18 @@ public class MapFragment extends Fragment {
         TextView tvLongitude = view.findViewById(R.id.longitude);
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
 
-        handler = new Handler();
-
-        Runnable runnable = new Runnable() {
+        LocationViewModel locationSensor = new ViewModelProvider(this, new ViewModelProvider.Factory() {
+            @NonNull
             @Override
-            public void run() {
-                tvLatitude.setText("위도 = " + locationSensor.getLatitude().getValue());
-                tvLongitude.setText("경도 = " + locationSensor.getLongitude().getValue());
-                handler.postDelayed(this, MIN_TIME_INTERVAL);
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new LocationViewModel(LocationSensor.getLocationSensor());
             }
-        };
+        }).get(LocationViewModel.class);
 
-        handler.postDelayed(runnable, MIN_TIME_INTERVAL);
+        locationSensor.getLatitude().observe(getViewLifecycleOwner(),
+                latitude -> tvLatitude.setText("위도 = " + latitude));
+        locationSensor.getLongitude().observe(getViewLifecycleOwner(),
+                longitude -> tvLongitude.setText("경도 = " + longitude));
 
         mapFragment.getMapAsync(new GoogleMapImpl());
 
