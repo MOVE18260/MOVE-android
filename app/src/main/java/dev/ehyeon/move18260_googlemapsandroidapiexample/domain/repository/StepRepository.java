b69998f1fa6cd48cf47dc8e7e8e12220caa97098
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-06-05 13:31:04 +0900

    feat: StepRepository 에 5초 마다 초기화 하는 기능 추가

--- app/src/main/java/dev/ehyeon/move18260_googlemapsandroidapiexample/domain/repository/StepRepository.java
+++ app/src/main/java/dev/ehyeon/move18260_googlemapsandroidapiexample/domain/repository/StepRepository.java
@@ -1,10 +1,15 @@
 package dev.ehyeon.move18260_googlemapsandroidapiexample.domain.repository;
 
+import android.os.CountDownTimer;
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 
 import dev.ehyeon.move18260_googlemapsandroidapiexample.data.step.StepSensor;
 
 public class StepRepository {
+
+    private static final String TAG = "StepRepository";
 
     private static StepRepository stepRepository = null;
 
@@ -12,6 +17,7 @@ public class StepRepository {
 
     private StepRepository(StepSensor stepSensor) {
         this.stepSensor = stepSensor;
+        startInitStepTimer();
     }
 
     public static StepRepository getStepRepository() {
@@ -32,5 +38,23 @@ public class StepRepository {
 
     public LiveData<Integer> getStep() {
         return stepSensor.getStep();
+    }
+
+    // TODO refactor
+    private void startInitStepTimer() {
+        // 총 10분, 5 초씩 onTick
+        new CountDownTimer(10 * 60 * 1000, 5 * 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                stepSensor.initStep();
+                Log.d(TAG, "init step");
+            }
+
+            @Override
+            public void onFinish() {
+                stepSensor.initStep();
+                start();
+            }
+        }.start();
     }
 }
