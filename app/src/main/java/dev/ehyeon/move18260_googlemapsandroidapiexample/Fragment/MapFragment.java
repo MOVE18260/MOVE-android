6cc6cca0faacbe466ae3519b29a0c2958cae802c
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-06-05 22:47:04 +0900

    refactor: 구글 맵 인터페이스와 fragment 를 분리할 수 없다고 판단, 병합

--- app/src/main/java/dev/ehyeon/move18260_googlemapsandroidapiexample/Fragment/MapFragment.java
+++ app/src/main/java/dev/ehyeon/move18260_googlemapsandroidapiexample/Fragment/MapFragment.java
@@ -1,5 +1,7 @@
 package dev.ehyeon.move18260_googlemapsandroidapiexample.Fragment;
 
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +13,24 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 import dev.ehyeon.move18260_googlemapsandroidapiexample.R;
 import dev.ehyeon.move18260_googlemapsandroidapiexample.data.location.LocationSensor;
 import dev.ehyeon.move18260_googlemapsandroidapiexample.presentation.viewmodel.LocationViewModel;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback,
+        OnMyLocationButtonClickListener, OnMyLocationClickListener {
+
+    private final LocationSensor locationSensor = LocationSensor.getLocationSensor();
+
+    private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -41,8 +54,33 @@ public class MapFragment extends Fragment {
         locationSensor.getLongitude().observe(getViewLifecycleOwner(),
                 longitude -> tvLongitude.setText("경도 = " + longitude));
 
-        mapFragment.getMapAsync(new GoogleMapImpl());
+        mapFragment.getMapAsync(this);
 
         return view;
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        googleMap.setMyLocationEnabled(true);
+        googleMap.setOnMyLocationButtonClickListener(this);
+        googleMap.setOnMyLocationClickListener(this);
+
+        Location lastKnownLocation = locationSensor.getLastKnownLocation();
+
+        // zoom = 15 == 반경 1.5km
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude()), 15));
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
     }
 }
